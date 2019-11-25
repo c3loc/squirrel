f151f046a6938eb95ad6ad820bf632d4124afc8b
Morre <morre@mor.re>
2019-11-25 15:32:28 +0100

    feat: add commit message linting

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,6 +11,12 @@ source venv/bin/activate
 pip install -r requirements.txt 1>/dev/null
 pip install -r requirements-dev.txt 1>/dev/null
 
+# Commitlint
+npm install @commitlint/cli@8.2.0 @commitlint/config-conventional@8.2.0
+
+# Lint last commit from history
+./node_modules/.bin/commitlint .
+
 # Run linting
 flake8 squirrel/
 
