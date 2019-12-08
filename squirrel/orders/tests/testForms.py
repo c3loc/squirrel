889061712120ba7959e0acddbeeefb2e523116c3
Hanspeter Spalinger <github@spahan.ch>
2019-12-08 12:52:38 +0000

    test: rewrote tests to test forms instead views

--- squirrel/orders/tests/testForms.py
+++ squirrel/orders/tests/testForms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Permission, User
 from django.test import TestCase
-from orders.models import Order, Product, Team
+from orders.forms import OrderForm
+from orders.models import Product, Team
 
 
 class OrderFormTests(TestCase):
@@ -8,7 +9,7 @@ class OrderFormTests(TestCase):
         User.objects.create_user(username="not_team_member", password="test123")
 
         self.user = User.objects.create_user(username="team_member", password="test123")
-        self.team = Team.objects.create(name="Aperture Science Laboratories")
+        self.team = Team.objects.create(name="Bottles")
         self.team.members.add(self.user)
         self.team.save()
 
@@ -18,22 +19,18 @@ class OrderFormTests(TestCase):
         self.product = Product.objects.create(name="Tardis", unit_price=17.00)
 
     def test_require_amount(self):
-        self.client.login(username="helpdesk", password="test123")
-        response = self.client.post(
-            "/orders/new",
-            {
-                "product": self.product.id,
-                "team": self.team.id,
-                "state": "REQ",
-                "unit_price": 10.00,
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(
-            response,
-            '<input type="number" name="amount" min="0" class="numberinput form-control is-invalid" required id="id_amount">',
-            html=True,
-        )
+        # TODO: move to model test
+        form_data = {
+            "product": self.product.id,
+            "team": self.team.id,
+            "state": "REQ",
+            "unit_price": 10.00,
+        }
+        form = OrderForm(data=form_data, teams=Team.objects.all())
+        self.assertFalse(form.is_valid())
+        form_data["amount"] = 1
+        form = OrderForm(data=form_data, teams=Team.objects.all())
+        self.assertTrue(form.is_valid())
 
     def test_require_product_or_suggestion(self):
         self.client.login(username="helpdesk", password="test123")
@@ -81,51 +78,34 @@ class OrderFormTests(TestCase):
         )
 
     def test_team_members_can_set_team(self):
-        self.client.login(username="team_member", password="test123")
-        response = self.client.post(
-            "/orders/new",
-            {
-                "amount": 1,
-                "product": self.product.id,
-                "team": self.team.id,
-                "state": "REQ",
-                "unit_price": 10.00,
-            },
-        )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Order.objects.all().count(), 1)
+        form_data = {
+            "amount": 1,
+            "product": self.product.id,
+            "team": self.team.id,
+            "state": "REQ",
+            "unit_price": 10.00,
+        }
+        form = OrderForm(data=form_data, teams=Team.objects.filter(id=self.team.id))
+        self.assertTrue(form.is_valid())
 
     def test_non_team_members_can_not_set_team(self):
-        self.client.login(username="not_team_member", password="test123")
-        response = self.client.post(
-            "/orders/new",
-            {
-                "amount": 1,
-                "product": self.product.id,
-                "team": self.team.id,
-                "state": "REQ",
-                "unit_price": 10.00,
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        # TODO: why does this fail, but not the one below? something about the select?
-        # self.assertContains(response, '<select name="team" class="select form-control is-invalid" required id="id_team">', html=True)
-        self.assertContains(
-            response,
-            b'<select name="team" class="select form-control is-invalid" required id="id_team">',
-        )
+        form_data = {
+            "amount": 1,
+            "product": self.product.id,
+            "team": self.team.id,
+            "state": "REQ",
+            "unit_price": 10.00,
+        }
+        form = OrderForm(data=form_data, teams=Team.objects.none())
+        self.assertFalse(form.is_valid())
 
-    def test_helpdesk_can_set_team(self):
-        self.client.login(username="helpdesk", password="test123")
-        response = self.client.post(
-            "/orders/new",
-            {
-                "amount": 1,
-                "product": self.product.id,
-                "team": self.team.id,
-                "state": "REQ",
-                "unit_price": 10.00,
-            },
-        )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Order.objects.all().count(), 1)
+    def test_valid_permission_can_set_team(self):
+        form_data = {
+            "amount": 1,
+            "product": self.product.id,
+            "team": self.team.id,
+            "state": "REQ",
+            "unit_price": 10.00,
+        }
+        form = OrderForm(data=form_data, teams=Team.objects.all())
+        self.assertTrue(form.is_valid())
